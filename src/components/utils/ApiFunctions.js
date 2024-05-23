cc36c6d1c3bd33d8452c6c61ae9c78d362d48b3f
MinMin <gow.bin.gg@gmail.com>
2024-05-23 19:36:34 +0700

    feat: create function to call [Login] API

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -27,3 +27,17 @@ export async function registerUser(registration) {
 	} 
 }
 
+/* This is function to login */
+export async function loginUser (login) {
+	try {
+		const response = await api.post("auth/login", login)
+		if(response.status >= 200 && response.status < 300) {
+			return response.data
+		} else {
+			return null
+		}
+	} catch (error) {
+		console.error(error)
+		return null
+	}
+}
