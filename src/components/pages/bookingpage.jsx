ceb3ba4c08efe66d5f66790265a80b2d548c0d7a
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-12-06 21:56:37 +0700

    ...

--- src/components/pages/bookingpage.jsx
+++ src/components/pages/bookingpage.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
-import { Calendar, Users, MapPin, Bed, CreditCard, ChevronRight, ChevronLeft } from 'lucide-react'
+import {Calendar, Users, MapPin, Bed, CreditCard, ChevronRight, ChevronLeft, CheckCircle, XCircle} from 'lucide-react'
 import '../styles/booking-page.css'
 import { getRoomTypes ,getAllBranches } from '../utils/ApiFunctions.js';
 import { bookRoom, getRoomsByBranchIdAndState} from '../utils/ApiFunctions.js'
+import {AnimatePresence, motion} from "framer-motion";
 
 const BookingPage = () => {
   const [step, setStep] = useState(1);
@@ -23,6 +24,38 @@ const BookingPage = () => {
   const [loading, setLoading] = useState(false);
   const [notification, setNotification] = useState({ show: false, message: '', type: '' });
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // eslint-disable-next-line react/prop-types
+  const Notification = ({ message, type, onClose }) => {
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            className={`fixed bottom-4 right-4 px-6 py-3 rounded-lg shadow-lg ${
+                type === 'success' ? 'bg-green-500' : 'bg-red-500'
+            } text-white flex items-center z-50`}
+        >
+          {type === 'success' ? (
+              <CheckCircle className="w-5 h-5 mr-2" />
+          ) : (
+              <XCircle className="w-5 h-5 mr-2" />
+          )}
+          <span>{message}</span>
+          <button
+              onClick={onClose}
+              className="ml-4 text-white hover:text-gray-200 focus:outline-none"
+          >
+            <XCircle className="w-4 h-4" />
+          </button>
+        </motion.div>
+    );
+  };
+
+  const handleCloseNotification = () => {
+    setNotification({ show: false, message: '', type: '' });
+  };
+
 
   const showNotification = (message, type) => {
     setNotification({ show: true, message, type });
@@ -490,8 +523,15 @@ const BookingPage = () => {
               )}
             </div>
           </form>
-
-
+          <AnimatePresence>
+            {notification.show && (
+                <Notification
+                    message={notification.message}
+                    type={notification.type}
+                    onClose={handleCloseNotification}
+                />
+            )}
+          </AnimatePresence>
         </div>
       </div>
   )
